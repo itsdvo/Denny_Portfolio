7ed7b15a93f8e1213ed5433e0424226fc0f6b15e
itsdvo <134029211+itsdvo@users.noreply.github.com>
2023-06-25 12:03:29 -0700

    Update EV_Project_2.0.sql

--- Project_1_Electric_Vehicles_WA/EV_Project_2.0.sql
+++ Project_1_Electric_Vehicles_WA/EV_Project_2.0.sql
@@ -1,9 +1,9 @@
 --	DATA SOURCE:	https://catalog.data.gov/dataset/electric-vehicle-population-data
 --	Title:			Electric Vehicle Population Data
 --	Meta Update:	May 13, 2023
---	Description:	This dataset shows the Battery Electric Vehicles (BEVs) and Plug-in Hybrid Electric Vehicles (PHEVs) that are currently registered through Washington State Department of Licensing (DOL).
+--	Description:	This dataset shows the Battery Electric Vehicles (BEVs) and Plug-in Hybrid Electric Vehicles (PHEVs) that are currently registered through the Washington State Department of Licensing (DOL).
 
---	Broke down the data into three tables, converted dates on excel, and assign entry_id as primary key
+--	Broke down the data into three tables, converted dates on Excel, and assign entry_id as the primary key
 
 --SELECT TOP (1000) [entry_id]
 --      ,[Date]
@@ -65,25 +65,25 @@ ORDER BY
 --	SYNTAX EXPLANATION
 
 --	SELECT clause:
-			--		Showed all columns with their respective alias to refer for future use
-			--		Applied CONVERT function to date_region.date in order to remove hours:minutes:seconds
+		--	Showed all columns with their respective alias to refer for future use
+		--	Applied CONVERT function to date_region.date in order to remove hours:minutes:seconds
 --	FROM clause:
-			--		Perform multiple inner joins on the primary key of entry_id
-			--		Assigned aliases for each table
+		--	Perform multiple inner joins on the primary key of entry_id
+		--	Assigned aliases for each table
 --	WHERE clause:
-			--		Trying to retrieve recent data since they were assigned with dates
-			--		Used a subquery on date_region.date
-				--		Applied the MAX function for highest value date, from EV_WA_Date_Region table
-			--		AND clause:
-				--		looking for cars registered originally in WA 
-				--		set date_region.state to retrieve string values that contain 'WA'
+		--	Trying to retrieve recent data since they were assigned with dates
+		--	Used a subquery on date_region.date
+		--	Applied the MAX function for highest value date, from EV_WA_Date_Region table
+		--	AND clause:
+			--	looking for cars registered originally in WA 
+			--	set date_region.state to retrieve string values that contain 'WA'
 --	ORDER BY clause:
-			--		Ascending on date_region.County so we can view in alphabetical order
+		--	Ascending on date_region.County so we can view in alphabetical order
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 --	QUESTION 1:
-			--		Which counties had the highest amount of electrical vehicles? Also form a CTE from the previous query
+		--	Which counties had the highest amount of electric vehicles? Also, form a CTE from the previous query
 
 WITH Update_EV_Table AS
 (
@@ -130,26 +130,26 @@ WHERE
 ;
 
 --	OUTPUT:
-			--		Displays King County, with EV Primary type being Passenger cars, with an EV_Total of 66704 as of 2023-04-30
+		--	Displays King County, with EV Primary type being Passenger cars, with an EV_Total of 66704 as of 2023-04-30
 
 --	SYNTAX EXPLANATION:
 
 --	WITH clause:
-			--		Known as a Common Table Expression (CTE), form a temporary table to make queries from
-			--		Assigned AS Update_EV_Table
-			--		Used the previous query but removed ORDER BY clause beauce it doesn't work with a CTE
+		--	Known as a Common Table Expression (CTE), it forms a temporary table to make queries from
+		--	Assigned AS Update_EV_Table
+		--	Used the previous query but removed the ORDER BY clause because it doesn't work with a CTE
 --	SELECT clause:
-			--		Show everything
+		--	Show everything
 --	FROM clause:
-			--		Query from the temporary table Update_EV_Table
+		--	Query from the temporary table Update_EV_Table
 --	WHERE clause:
-			--		Used a similar subquery from Max date but with EV_Total instead
-			--		FROM the temporary table Update_EV_Table
+		--	Used a similar subquery from Max date but with EV_Total instead
+		--	FROM the temporary table Update_EV_Table
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 --	QUESTION 2:
-			--		What are the top 5 counties that have the highest totals in EV, and calculate the percentage they are in within the county EV population
+		--	What are the top 5 counties that have the highest totals in EV, and calculate the percentage they are in within the county EV population
 
 WITH Update_EV_Table AS
 (
@@ -195,30 +195,30 @@ ORDER BY
     EV_Total DESC;
 ;
 --	OUTPUT:
-			--	Top 5 counties are King, Snohomish, Pierce, Clark, and Spokane
-			--	Most EV are battery powered ranging from 67% to 80% across all 5 counties
-			--	Car manufacturers are investing more into battery vs hybrid, trend for future
+		--	Top 5 counties are King, Snohomish, Pierce, Clark, and Spokane
+		--	Most EVs are battery powered ranging from 67% to 80% across all 5 counties
+		--	Car manufacturers are investing more in battery vs hybrid, a trend for future
 
 --	SYNTAX EXPLANATION:
 
 --	WITH clause:
-			--	Used previous CTE
+		--	Used previous CTE
 --	SELECT clause:
-			--	TOP 5 for only the first nth results
-			--	Added two percentage columns for battery and hybrid vehicles
-			--	CASE statement to exclude null or 0 values
-				--	Basically saying if value is not 0 then do the arithmitic of vehicle equipment type divided by EV_Total
-			--	ROUND function wrapped around the case statement with 4 digits because I multiply by 100 to give a presentable percent value
+		--	TOP 5 for only the first nth results
+		--	Added two percentage columns for battery and hybrid vehicles
+		--	CASE statement to exclude null or 0 values
+			--	Basically saying if the value is not 0 then do the arithmetic of vehicle equipment type divided by EV_Total
+		--	ROUND function wrapped around the case statement with 4 digits because I multiply by 100 to give a presentable percent value
 --	FROM clause:
-			--	CTE table
+		--	CTE table
 --	ORDER BY clause:
-			--	Want to see the highest EV_Total descending
+		--	Want to see the highest EV_Total descending
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 --	QUESTION 3:
-			--	How has EV Population for King, Snohomish, and Pierce counties change over time? Also what is their percentage of EV on their respective county population.
+		--	How has the EV Population for King, Snohomish, and Pierce counties changed? Also, what is the percentage of EVs in their respective county population?
 
 SELECT
 	CONVERT(date, date_region.date) AS year_month_date,
@@ -250,30 +250,30 @@ ORDER BY
 	County
 ;
 --	OUTPUT:
-			--	Shows King, Pierce, and Snohomish battery, hybrids, total EV, and total vehicles, and EV percentage to population YTD
-			--	GROUP BY year in ascending order (Oldest->Newest)
+		--	Shows King, Pierce, and Snohomish battery, hybrids, total EV, total vehicles, and EV percentage to population YTD
+		--	GROUP BY year in ascending order (Oldest->Newest)
 
 --	SYNTAX EXPLANATION:
 
 --	SELECT clause:
-			--	CONVERTED date, county, region
-			--	SUM function on Battery, Hybrid, EV, and total Vehicles
-			--	SUM EV / SUM Total for percentage, multiplied 100 for percentage and then ROUNDED to 2 decimals
+		--	CONVERTED date, county, region
+		--	SUM function on Battery, Hybrid, EV, and total Vehicles
+		--	SUM EV / SUM Total for percentage multiplied by 100 for percentage, and then ROUNDED to 2 decimals
 --	FROM clause:
-			--  Perform same JOINS from before using the primary key 'entry_id'
+		--  Perform the same JOINS from before using the primary key 'entry_id'
 --	WHERE clause:
-			--	County we us IN to include string values that match either King, Snohomish, or Pierce
+		--	County we use IN to include string values that match either King, Snohomish, or Pierce
 --	GROUP BY clause:
-			--	for our non aggregate columns we have to include in our GROUP BY statement
+		--	For our non-aggregate columns we have to include them in our GROUP BY statement
 --	ORDER BY clause:
-			--	Sort by year first (default ascending) so we oldest dates first, then sort by County (default ascending)
+		--	Sort by year first (default ascending) so we oldest dates first, then sort by County (default ascending)
 
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 -- QUESTION 4:
-			--	What is the fourth highest Electric Vehicle Population county that registered through Washington State Department of Licensing (DOL) of recent year?
+		--	What is the fourth highest Electric Vehicle Population county that registered through the Washington State Department of Licensing (DOL) in recent years
 
 WITH DENSE_CTE AS
 (
@@ -314,21 +314,21 @@ WHERE
 ;
 
 --	OUTPUT:
-			--	Displays Clark county as the 4th listed with Dense Rank of Electric Vehicle Totals within the whole population
+		--	Displays Clark County as the 4th listed with a Dense Rank of Electric Vehicle Totals within the whole population
 
 --	SYNTAX EXPLANATION:
 			
 --	CTE:
-			--	SELECT clause:
-						--	Set it by DISTINCT county for no repeats
-						--	Retrieve State in case other states would rank in fourth
-						--	SUM BEV, HEV and Total EVs
-						--	Apply DENSE RANK OVER the sum totals of EV Vehicles
-							--	DESC order for largest to lowest when applying DENSE RANK
+		--	SELECT clause:
+			--	Set it by DISTINCT county for no repeats
+			--	Retrieve State in case other states would rank in fourth
+			--	SUM BEV, HEV and Total EVs
+			--	Apply DENSE RANK OVER the sum totals of EV Vehicles
+				--	DESC order for largest to lowest when applying DENSE RANK
 --	SELECT clause:
-			--	Everything
+		--	Everything
 --	FROM clause:
-			--	From CTE we assigned as DENSE_CTE
+		--	From CTE we assigned as DENSE_CTE
 --	WHERE clause:
-			--	Set it from DENSERANK column equals to 4 therefore grabbing the 4th rank
+		--	Set it from the DENSERANK column equal to 4, therefore, grabbing the 4th rank
 
